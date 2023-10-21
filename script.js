953e1c2994a43acdc793cc94dbf732aeb2f250f3
Muraliprasad <111122436+MURALIPRASADM@users.noreply.github.com>
2023-10-21 14:06:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+const items=[];
+const operate=[];
+let index=-1;
+var ind=0;
+function add() {
+    let value = document.getElementById("add-items").value;
+    if(!value.trim()=='') {
+    items.push(value);
+     index++;
+     display(index);
+    }
+}
+
+function display(index) {
+    if(index!=items.length)
+    {
+     const elements = document.createElement("div");
+     elements.innerHTML =items[index];
+     document.getElementById("display").appendChild(elements); 
+    }  
+}
+
+function removeItem() {
+    const remelement=document.getElementById("remove-items").value;
+    const ind=items.indexOf(remelement);
+    index--;
+    document.getElementById("display").innerHTML="";
+    if(ind==-1)
+    {
+        alert("Element not found");
+        for(let j=0;j<items.length;j++)
+        display(j);
+    }
+    else
+    {
+    items.splice(ind,1);
+    for(let j=0;j<items.length;j++)
+    display(j);
+    }
+}
+
+
